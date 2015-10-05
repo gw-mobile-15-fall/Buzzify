7fd460084e0a7322af04062253e00d33820cdeac
Aaron <nop@gwmail.gwu.edu>
2015-10-04 22:56:53 -0400

    Wrote some of LoginActivity.

--- Buzzify/app/src/main/java/edu/gwu/buzzify/LoginActivity.java
+++ Buzzify/app/src/main/java/edu/gwu/buzzify/LoginActivity.java
@@ -1,12 +1,20 @@
 package edu.gwu.buzzify;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button mBtnSignIn;
+    private Button mBtnRegister;
+    private String mUserName;
+    private String mPassword;
     private static final String TAG = "LoginActivity";
 
     @Override
@@ -16,6 +24,31 @@ public class LoginActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mBtnSignIn = (Button) findViewById(R.id.btnSignIn);
+        mBtnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (v == mBtnSignIn) {
+                    TextView userText = (TextView) findViewById(R.id.etUsername);
+                    mUserName = userText.getText().toString();
+
+                    TextView passwordText = (TextView) findViewById(R.id.etPassword);
+                    mPassword = passwordText.getText().toString();
+
+                    //Check for correct login credentials. Display error if invalid.
+                    if (!(mUserName.matches(getResources().getString(R.string.username)) && mPassword.matches(getResources().getString(R.string.password))) {
+                        Toast.makeText(LoginActivity.this, R.string.loginErrorMsg, Toast.LENGTH_SHORT).show();
+                    }
+
+                    //Change to location view.
+                    Intent intentLocation = new Intent(LoginActivity.this, LoginActivity.class);
+                    LoginActivity.this.startActivity(intentLocation);
+                }
+
+            }
+        });
     }
 
 
